23d3de014541fc74a8b829649ea4ec38122872d9
Alexey Ilyichev <alexey.ilyichev@qik.com>
2010-11-13 13:51:50 +0300

    Added @image which inserts an image into the resource header

--- lib/yard-rest-plugin.rb
+++ lib/yard-rest-plugin.rb
@@ -18,4 +18,5 @@ YARD::Tags::Library.define_tag("Example Response", :example_response)
 YARD::Tags::Library.define_tag("Response Fields", :response_field, :with_types_and_name)
 YARD::Tags::Library.define_tag("Headers", :header, :with_name)
 YARD::Tags::Library.define_tag("Response codes", :response_code, :with_name)
+YARD::Tags::Library.define_tag("Image", :image)
 
