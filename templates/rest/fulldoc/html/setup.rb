29e028f994da5e4510fb01b327b4bf1b652f573d
rknLA <kevin@rkn.la>
2012-01-26 23:52:28 -0800

    Update fulldoc setup to new format.
    Mostly this was me just deleting things.  Since I'm still not getting a
    doc folder output, it's possible that this has broken things further,
    but until I figure that out for sure, I'll consider deleting things to
    be progress.

--- templates/rest/fulldoc/html/setup.rb
+++ templates/rest/fulldoc/html/setup.rb
@@ -7,20 +7,24 @@ end
 
 
 def generate_topic_list
-  topic_objects = index_objects(options[:objects]).reject { |o| o.root? }
-  @topics = {}
+  @items = Registry.all(:topic)
+  @list_title = 'Topic List'
+  @list_type = 'topic'
 
-  topic_objects.each do |object|
-    object.tags('topic').each { |topic| (@topics[topic.text] ||= []) << object }
-  end
+  #topic_objects = index_objects(options[:objects]).reject { |o| o.root? }
+  #@topics = {}
 
-  @list_title = "Topic List"
-  @list_type = "topic"
+  #topic_objects.each do |object|
+  #  object.tags('topic').each { |topic| (@topics[topic.text] ||= []) << object }
+  #end
+
+  #@list_title = "Topic List"
+  #@list_type = "topic"
   asset('topic_list.html', erb(:full_list))
 end
 
 def generate_resource_list
-  @items = index_objects(options[:objects])
+  @items = Registry.all(:resource)
   @list_title = "Resource List"
   @list_type = "resource"
   asset('resource_list.html', erb(:full_list))
