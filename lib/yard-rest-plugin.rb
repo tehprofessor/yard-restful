517d87a9285c16d80b748380276f6c2bf3647579
rknLA <kevin@rkn.la>
2012-01-26 19:22:34 -0800

    Fix ambiguity between Request Field and Arguments.
    Also added distinction between required and optional arguments

--- lib/yard-rest-plugin.rb
+++ lib/yard-rest-plugin.rb
@@ -11,9 +11,9 @@ YARD::Templates::Engine.register_template_path File.dirname(__FILE__) + '/../tem
 # Define custom tags
 YARD::Tags::Library.define_tag("URL for Service", :url)
 YARD::Tags::Library.define_tag("Topic for Service", :topic)
-YARD::Tags::Library.define_tag("Arguments", :argument, :with_types_and_name)
+YARD::Tags::Library.define_tag("Required Arguments", :argument, :with_types_and_name)
+YARD::Tags::Library.define_tag("Optional Arguments", :optional_argument, :with_types_and_name)
 YARD::Tags::Library.define_tag("Example Request", :example_request)
-YARD::Tags::Library.define_tag("Request Fields", :request_field, :with_types_and_name)
 YARD::Tags::Library.define_tag("Example Response", :example_response)
 YARD::Tags::Library.define_tag("Response Fields", :response_field, :with_types_and_name)
 YARD::Tags::Library.define_tag("Headers", :header, :with_name)
