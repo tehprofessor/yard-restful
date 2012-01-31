b386bebf06849996ac280ab8495eadd5e3be6077
rknLA <kevin@rkn.la>
2012-01-31 15:16:08 -0800

    Remove everything except fulldoc

--- templates/rest/layout/html/setup.rb
+++
@@ -1,9 +0,0 @@
-include Helpers::FilterHelper
-
-
-def menu_lists
-  [ { :type => 'resource', :title => "Resources", :search_title => "Resource List" },
-    { :type => 'topic', :title => "Topics", :search_title => "Topic List" },
-    { :type => 'file', :title => "Files", :search_title => "File List" } ]
-end
-
