be3dc37cfee9b593f661b895196786b1a73eb900
Konstantin Rafalsky <kraft001@gmail.com>
2012-10-10 12:02:36 +0400

    Return @url tag and customize the methods list

--- lib/yard-rest/tags.rb
+++ lib/yard-rest/tags.rb
@@ -3,6 +3,7 @@ YARD::Tags::Library.define_tag("URL for the Resource",          :resource)
 YARD::Tags::Library.define_tag("Object Used in Resource",       :resource_object)
 YARD::Tags::Library.define_tag("Resource Object Properties",    :resource_object_properties)
 YARD::Tags::Library.define_tag("Resource Object Footnotes",     :resource_object_footnotes)
+YARD::Tags::Library.define_tag("URL",                           :url)
 YARD::Tags::Library.define_tag("HTTP-Action for the Resource",  :action)
 
 YARD::Tags::Library.define_tag("Required Arguments",            :required,          :with_types_and_name)
