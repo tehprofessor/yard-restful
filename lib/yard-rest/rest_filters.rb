feb7e8a101f6aecab048bed656ed6b7fc60c5bb8
Konstantin Rafalsky <kraft001@gmail.com>
2012-10-11 14:44:05 +0400

    Move sorting to filters

--- lib/yard-rest/rest_filters.rb
+++ lib/yard-rest/rest_filters.rb
@@ -1,14 +1,18 @@
 module RestFilters
 
   def select_objects(list)
-    list.select{ |item| item.has_tag?(:resource_object) } if list
+    if list
+      list.
+        select{ |item| item.has_tag?(:resource_object) }.
+        sort_by { |s| s.name.to_s }
+    end
   end
 
   def select_resources(list)
     if list
-      list.select do |object|
-        object.meths.any?{ |x| x.has_tag?(:resource) }
-      end
+      list.
+        select{ |object| object.meths.any?{ |x| x.has_tag?(:resource) } }.
+        sort_by { |s| s.name.to_s }
     end
   end
 
