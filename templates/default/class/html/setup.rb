cfb03595eae82ae234c2cfefb876e1e94693fc94
Konstantin Rafalsky <kraft001@gmail.com>
2012-10-29 09:19:18 +0400

    Better style for long docstring

--- templates/default/class/html/setup.rb
+++ templates/default/class/html/setup.rb
@@ -1,6 +1,5 @@
 def init
   @page_title = "#{object.name.to_s.gsub(/Controller/,"")} - #{options[:title]}"
-  #sections :header, [T('docstring'), :method_details_list, [:fields_list], [T('method_details')]]
   sections :header, [T('docstring'), :object_details, [:fields_list], :resource_details, [:fields_list]]
 end
 
